d73aea3e25fbdb366eb2b138b02eb35991cd8d17
neelsebastian44 <31883746-neelsebastian44@users.noreply.replit.com>
2024-02-11 21:05:25 +0000

    updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Tip Calculator Project
 print("Welcome to the tip calculator.")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
